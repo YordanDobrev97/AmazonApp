b891e4341291daef8d58c6dfd7a5e4a8dc5a462e
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2021-12-11 22:14:29 +0200

    Add Quantity property to Product model

--- AmazonSystem.Data/Migrations/20211211200938_AddQuantityProductProperty.Designer.cs
+++ AmazonSystem.Data/Migrations/20211211200938_AddQuantityProductProperty.Designer.cs
@@ -4,14 +4,16 @@ using AmazonSystem.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AmazonSystem.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211211200938_AddQuantityProductProperty")]
+    partial class AddQuantityProductProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AmazonSystem.Data/Migrations/20211211200938_AddQuantityProductProperty.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AmazonSystem.Data.Migrations
+{
+    public partial class AddQuantityProductProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Products");
+        }
+    }
+}
