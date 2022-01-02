0948468e5236c7f8c6f60510b64d4746620d9287
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2022-01-02 19:03:33 +0200

    Add Sale Entities

---
+++ AmazonSystem.Data/Migrations/20220102165515_AddSaleEntities.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AmazonSystem.Data.Migrations
+{
+    public partial class AddSaleEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+               name: "SaleId",
+               table: "Products",
+               type: "int",
+               nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Sales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    IsSuccess = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sales_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SaleId",
+                table: "Products",
+                column: "SaleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_UserId",
+                table: "Sales",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Sales_SaleId",
+                table: "Products",
+                column: "SaleId",
+                principalTable: "Sales",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Sales_SaleId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_AspNetUsers_UserId",
+                table: "Sales");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sales_UserId",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "IsSuccess",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Sales");
+
+            migrationBuilder.RenameColumn(
+                name: "SaleId",
+                table: "Products",
+                newName: "SaleProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_SaleId",
+                table: "Products",
+                newName: "IX_Products_SaleProductId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Sales",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "SaleProducts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SaleId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    IsSuccess = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SaleProducts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SaleProducts_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SaleProducts_Sales_SaleId",
+                        column: x => x.SaleId,
+                        principalTable: "Sales",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleProducts_SaleId",
+                table: "SaleProducts",
+                column: "SaleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleProducts_UserId",
+                table: "SaleProducts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_SaleProducts_SaleProductId",
+                table: "Products",
+                column: "SaleProductId",
+                principalTable: "SaleProducts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_SaleProducts_SaleProductId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "SaleProducts");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Sales");
+
+            migrationBuilder.RenameColumn(
+                name: "SaleProductId",
+                table: "Products",
+                newName: "SaleId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_SaleProductId",
+                table: "Products",
+                newName: "IX_Products_SaleId");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSuccess",
+                table: "Sales",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Sales",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_UserId",
+                table: "Sales",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Sales_SaleId",
+                table: "Products",
+                column: "SaleId",
+                principalTable: "Sales",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_AspNetUsers_UserId",
+                table: "Sales",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ AmazonSystem.Data/Models/Sale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AmazonSystem.Data.Models
+{
+    public class Sale
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
